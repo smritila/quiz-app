61ae2aeb0bb4732be681ddaed35f21cd7f5c93d9
Smriti <smrtghoshal24@gmail.com>
2023-03-23 18:15:19 +0530

    chore: "implement state in trivia component"

---
+++ src/App.jsx
@@ -0,0 +1,135 @@
+import "./app.css";
+import { useState } from "react";
+import Trivia from "./components/Trivia";
+function App() {
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [timeOut, setTimeOut] = useState(false);
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+
+    {
+      id: 2,
+      question: "when did the website Facebook launch",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+
+        {
+          text: "2006",
+          correct: false,
+        },
+
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "who is the president of India?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Oenzel Washington",
+          correct: false,
+        },
+
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
+  const moneyPyramid = [
+    { id: 1, amount: "$ 100" },
+    { id: 2, amount: "$ 200" },
+    { id: 3, amount: "$ 300" },
+    { id: 4, amount: "$ 500" },
+    { id: 5, amount: "$ 1000" },
+    { id: 6, amount: "$ 2000" },
+    { id: 7, amount: "$ 4000" },
+    { id: 8, amount: "$ 8000" },
+    { id: 9, amount: "$ 16000" },
+    { id: 10, amount: "$ 32000" },
+    { id: 11, amount: "$ 64000" },
+    { id: 12, amount: "$ 12500" },
+    { id: 13, amount: "$ 1250000" },
+    { id: 14, amount: "$ 500000" },
+    { id: 15, amount: "$ 1000000" },
+  ].reverse();
+
+  return (
+    <div className="app">
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Trivia
+            data={data}
+            setTimeOut={setTimeOut}
+            questionNumber={questionNumber}
+            setQuestionNumber={setQuestionNumber}
+          />
+        </div>
+      </div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid.map((m) => (
+            <li
+              className={
+                questionNumber === m.id
+                  ? "moneyListItem active"
+                  : "moneyListItem"
+              }
+            >
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
